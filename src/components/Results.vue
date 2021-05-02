fef43bc90b44385a9fd2d91e369133c23b234ff1
dnhmphrs <45633295+dnhmphrs@users.noreply.github.com>
2021-05-02 21:21:18 +0100

    edits

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="outer">
     <div class="results">
-      <iframe src="https://open.spotify.com/embed/track/6epn3r7S14KUqlReYr77hA" width="250" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <iframe src="https://open.spotify.com/embed/track/08td7MxkoHQkXnWAYD8d6Q" width="250" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
       <div class="break" />
       <button class="return" @click="goToSelection()">try again</button>
     </div>
