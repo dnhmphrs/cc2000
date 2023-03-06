14427e62aff5038a63f293e6488505cc5b38838d
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-03-06 21:30:50 +0000

    edits

---
+++ WEB/src/routes/the-past/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+	let handleProgress = () => {
+		page.set(1);
+	};
+</script>
+
+<svelte:head>
+	<title>CC2000 | 404</title>
+</svelte:head>
+<section>
+	<img src="/gifs/the-past.gif" alt="404 error" />
+	<h1 on:click={() => handleProgress()}>
+		You were born in the time of dinosaurs, there was no music.
+	</h1>
+</section>
+
+<style>
+	section {
+		width: 100%;
+		height: 100%;
+		display: flex;
+	}
+
+	h1 {
+		text-align: center;
+
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		background: black;
+		padding: 0.5rem 1rem;
+
+		border: solid 1px var(--white-50);
+		color: var(--white-50);
+
+		cursor: pointer;
+	}
+
+	h1:hover {
+		border-color: var(--white);
+		color: var(--white);
+	}
+</style>
