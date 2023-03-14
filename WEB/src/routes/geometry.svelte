e0bc6fc8bfd2649326570a6cc878b2661fe0aaaa
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-03-14 13:40:20 +0000

    edit

--- WEB/src/routes/geometry.svelte
+++ WEB/src/routes/geometry.svelte
@@ -198,7 +198,7 @@
 		// }
 
 		// camera.position.z -= deltaTime * 20;
-		camera.position.z -= deltaTime * 12.75;
+		camera.position.z -= deltaTime * 12.725;
 		gridHelper0.rotation.y -= deltaTime / 10;
 		gridHelper1.rotation.y += deltaTime / 10;
 		gridHelper2.rotation.y -= deltaTime / 10;
